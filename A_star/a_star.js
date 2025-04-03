98d3de23193b5fef07f9c92bfce33736e531f8dc
Mikhail Golikov <misha-golikov2006@mail.ru>
2025-04-03 12:41:06 +0700

    сделал поиск пути от старта до финиша

--- A_star/a_star.js
+++ A_star/a_star.js
@@ -108,3 +108,93 @@ document.getElementById('randomizePoints').addEventListener('click', function() 
     randomizeStartEnd();
     drawGrid();
 });
+
+function findPath(){
+    let notUsedList = [];
+    let usedList = [];
+
+
+    function heuristic(start,finish){//эвристика
+        return Math.abs(start.x - finish.x) + Math.abs(start.y - finish.y)// стическая оценка
+
+    }
+    let firstCell = {
+        x:start.x,
+        y:start.y,
+        g:0,
+        h:heuristic(start, end),
+        f: 0 + heuristic(start, end),
+        parent: null
+    }
+    notUsedList.push(firstCell);
+
+    while(notUsedList.length > 0){
+        let minCost = notUsedList.reduce((minCell,currentCell) => {
+            return (currentCell.f < minCell.f) ? currentCell:minCell;
+        });
+        usedList.push(minCost);
+        notUsedList = notUsedList.filter(cell => cell !== minCost);
+
+        if(minCost.x === end.x && minCost.y === end.y){
+            let nowCell = minCost;
+            let Path = [];
+
+            while(nowCell != null){
+                Path.unshift({x:nowCell.x,y:nowCell.y});
+                nowCell = nowCell.parent;
+            }
+            console.log("Путь найден" , Path);
+            drowPath(Path);
+            break;
+        }
+
+        let neighbors = [
+            {x:minCost.x,y:minCost.y - 1},
+            {x:minCost.x,y:minCost.y + 1},
+            {x:minCost.x - 1,y:minCost.y},
+            {x:minCost.x + 1,y:minCost.y},
+        ];
+
+        for(let neighbor of neighbors){
+            if(neighbor.x >=0 && neighbor.x < cols && neighbor.y >= 0 && neighbor.y < rows){
+                if(maze[neighbor.y][neighbor.x] === 0){
+                    let g = minCost.g + 1;
+                    let h = heuristic(neighbor,end);
+                    let f = g + h;
+
+                    if(!usedList.some(cell => cell.x === neighbor.x && cell.y === neighbor.y)){
+                        let currentNeighbor = notUsedList.find(cell => cell.x === neighbor.x && cell.y === neighbor.y);
+                        if(currentNeighbor){
+                            if(f < currentNeighbor.f){
+                                currentNeighbor.g = g;
+                                currentNeighbor.h = h;
+                                currentNeighbor.f = f;
+                                currentNeighbor.parent = minCost;
+                            }
+
+                        }
+                        else {
+                            notUsedList.push({x:neighbor.x,y:neighbor.y,g:g,h:h,f:f,parent:minCost});
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+function drowPath(Path){
+    ctx.fillStyle = "green";
+    Path.forEach(cell => {
+        const x = cell.x * cellWidth;
+        const y = cell.y * cellHeight;
+        ctx.fillRect(x, y, cellWidth, cellHeight);
+    })
+}
+
+document.getElementById('findPath').addEventListener('click', function () {
+    findPath();
+});
+
+
